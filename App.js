3370de5d90b1417e0be9c5e8ea4e8a21c750516c
Nata Vacheishvili <natashkela@gmail.com>
2023-04-24 23:00:53 -0400

    borders lesson code

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>Hello World!</Text>
+      <View
+        style={{
+          borderWidth: 1, // Sets the width of all four borders of the container to 1
+          borderTopWidth: 5, // Sets the width of the top border of the container to 5
+          borderBottomWidth: 10, // Sets the width of the bottom border of the container to 10
+          borderLeftWidth: 3, // Sets the width of the left border of the container to 3
+          borderRightWidth: 20, // Sets the width of the right border of the container to 20
+          borderColor: '#15099d', // Sets the color of all four borders to a shade of blue
+          borderRadius: 10, // Sets the radius of all four border corners to 10 , making the border rounded
+          borderTopLeftRadius: 20, // Sets the radius of the top left border corner to 20
+          borderBottomLeftRadius: 20, // Sets the radius of the bottom left border corner to 20
+          borderTopRightRadius: 30, // Sets the radius of the top right border corner to 30
+          borderBottomRightRadius: 30, // Sets the radius of the bottom right border corner to 30
+        }}>
+        <Text>Hello World!</Text>
+      </View>
     </SafeAreaView>
   );
 };
