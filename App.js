be859940ddeb121f25808c5fa0e7139e4ffb2c67
Nata Vacheishvili <natashkela@gmail.com>
2023-04-26 19:27:03 -0400

    what is a component - done

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
-
+import {SafeAreaView} from 'react-native';
+import MyText from './components/MyText/MyText';
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>Hello World!</Text>
+      <MyText />
     </SafeAreaView>
   );
 };
---
+++ components/MyText/MyText.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const MyText = () => {
+  return <Text>Hello, React Native World!</Text>;
+};
+
+export default MyText;
