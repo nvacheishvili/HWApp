e1fe3da15acf91b7115b4bc5cd3e82c1ea2b704d
Nata Vacheishvili <natashkela@gmail.com>
2023-04-25 20:46:42 -0400

    done

--- App.js
+++ App.js
@@ -4,28 +4,41 @@ import {SafeAreaView, Text, View} from 'react-native';
 const App = () => {
   return (
     <SafeAreaView>
-      <View
-        style={{
-          backgroundColor: 'red', // sets the background color of the View container to red
-          margin: 10, // sets the margin around the View component to 10 on all sides
-          marginTop: 5, // sets the margin on the top side of the View component to 5 -- disable this for marginVertical to take effect
-          marginLeft: 100, // sets the margin on the left side of the View component to 100 -- disable this for marginHorizontal to take effect
-          marginRight: 100, // sets the margin on the right side of the View component to 100 -- disable this for marginHorizontal to take effect
-          marginBottom: 100, // sets the margin on the bottom side of the View component to 100 -- disable this for marginVertical to take effect
-          marginVertical: 50, // sets the vertical margin (top and bottom) of the View component to 50
-          marginHorizontal: 50, // sets the horizontal margin (left and right) of the View component to 50
-          padding: 10, // sets the padding inside the View component to 10 on all sides
-          paddingTop: 100, // sets the padding on the top side inside the View component to 100 -- disable this for paddingVertical to take effect
-          paddingLeft: 100, // sets the padding on the left side inside the View component to 100 -- disable this for paddingHorizontal to take effect
-          paddingRight: 10, // sets the padding on the right side inside the View component to 10 -- disable this for paddingHorizontal to take effect
-          paddingBottom: 100, // sets the padding on the bottom side inside the View component to 100 -- disable this for paddingVertical to take effect
-          paddingVertical: 50, // sets the vertical padding (top and bottom) inside the View component to 50
-          paddingHorizontal: 50, // sets the horizontal padding (left and right) inside the View component to 50
-        }}>
-        <Text>Hello World!</Text>
-      </View>
-      <View style={{backgroundColor: 'red'}}>
-        <Text>Hello World 2!</Text>
+      <View style={{position: 'relative', backgroundColor: 'green'}}>
+        <View
+          style={{
+            backgroundColor: 'yellow',
+            margin: 10,
+            marginTop: 5,
+            marginLeft: 100,
+            marginRight: 100,
+            marginBottom: 100,
+            marginVertical: 50,
+            marginHorizontal: 50,
+            padding: 10,
+            paddingTop: 100,
+            paddingLeft: 100,
+            paddingRight: 10,
+            paddingBottom: 100,
+            paddingVertical: 50,
+            paddingHorizontal: 50,
+            position: 'relative',
+            zIndex: 1,
+          }}>
+          <Text>Hello World!</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: 'red',
+            position: 'absolute',
+            top: 10,
+            left: 5,
+            right: 0,
+            bottom: 10,
+            zIndex: 2,
+          }}>
+          <Text>Hello World 2!</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
