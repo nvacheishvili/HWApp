731ac30f2e1e6dbd0febbdb33c6edf401eb534f1
Nata Vacheishvili <natashkela@gmail.com>
2023-04-26 18:51:24 -0400

    text styles

--- App.js
+++ App.js
@@ -4,7 +4,18 @@ import {SafeAreaView, Text} from 'react-native';
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>Hello World!</Text>
+      <Text
+        style={{
+          color: '#ff0000',
+          fontFamily: 'Arial',
+          fontSize: 20,
+          fontStyle: 'italic',
+          fontWeight: '100',
+          lineHeight: 70,
+          textAlign: 'right',
+        }}>
+        Hello World!
+      </Text>
     </SafeAreaView>
   );
 };
