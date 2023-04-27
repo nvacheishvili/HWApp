8d3d51e33951a451f92a7ebd3687aaeae6140a18
Nata Vacheishvili <natashkela@gmail.com>
2023-04-27 12:35:14 -0400

    inline vs stylesheet styles

--- components/MyText/MyText.js
+++ components/MyText/MyText.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import {Text} from 'react-native';
-
+import style from './style';
 const MyText = () => {
-  return <Text>Hello, React Native World!</Text>;
+  return (
+    <Text style={[style.text, style.text1, {color: 'green'}]}>
+      Hello, React Native World!
+    </Text>
+  );
 };
 
 export default MyText;
---
+++ components/MyText/style.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+const style = StyleSheet.create({
+  text: {
+    color: 'red',
+  },
+  text1: {
+    color: 'blue',
+  },
+});
+export default style;
