7b81014201e1cfac5eb5e13d75c27ff80a32635d
Nata Vacheishvili <natashkela@gmail.com>
2023-04-28 14:52:49 -0400

    view component basics

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View, Text} from 'react-native';
 import MyText from './components/MyText/MyText';
 const App = () => {
   return (
     <SafeAreaView>
-      <MyText />
+      <View style={{backgroundColor: 'blue', height: 100}}>
+        <Text>This is going to be our header container</Text>
+      </View>
+      <View style={{backgroundColor: 'grey'}}>
+        <MyText />
+        <MyText />
+        <MyText />
+      </View>
+      <View>
+        <Text>All rights reserved</Text>
+      </View>
     </SafeAreaView>
   );
 };
