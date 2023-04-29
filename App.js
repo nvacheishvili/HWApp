57d8e0e0dde721c5a31d8e987103f2353aac93f2
Nata Vacheishvili <natashkela@gmail.com>
2023-04-29 15:24:35 -0400

    simple examples of useEffect hook

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaView, Text} from 'react-native';
 
 const App = () => {
+  const [text, setText] = useState(0);
+
+  useEffect(() => {
+    //use effect that runs every time text value changes
+    console.log('The text has changed!');
+  }, [text]);
+
+  useEffect(() => {
+    //use effect that runs every time screen is rendered
+    console.log('The component has rendered!');
+  }, []);
+
   return (
     <SafeAreaView>
-      <Text>Hello World!</Text>
+      {/*on press change the text variable that in turn causes the useEffect to run*/}
+      <Text onPress={() => setText(text + 1)}>{text}</Text>
     </SafeAreaView>
   );
 };
