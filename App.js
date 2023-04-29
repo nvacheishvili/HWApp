3dbc5f80461dabe75864ca295ffb05725e7b9f6e
Nata Vacheishvili <natashkela@gmail.com>
2023-04-29 15:34:14 -0400

    use ref hook explained

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useRef} from 'react';
+import {SafeAreaView, Text, ScrollView, Button} from 'react-native';
 
 const App = () => {
+  // Create an array with 1000 elements, all initialized to 0.
+  let array = Array(1000).fill(0);
+
+  // Create a ref object using the useRef hook and initialize it to null.
+  // This ref will be used to reference the ScrollView component.
+  const scrollViewRef = useRef(null);
+
+  // Define a click handler function that will be called when the user clicks the button.
+  const handleClick = () => {
+    // Call the scrollTo method on the scrollViewRef.current object to scroll the ScrollView to the top.
+    scrollViewRef.current.scrollTo({x: 0, y: 0, animated: true});
+  };
+
+  // Render the SafeAreaView component and its children.
   return (
     <SafeAreaView>
-      <Text>Hello World!</Text>
+      {/* Render the ScrollView component and pass the scrollViewRef object as a ref prop. */}
+      <ScrollView ref={scrollViewRef}>
+        {/* Render the Text components for each element in the array. */}
+        {array.map((value, index) => (
+          <Text key={index}>Hello World {index}!</Text>
+        ))}
+      </ScrollView>
+      {/* Render the Button component and pass the handleClick function as an onPress prop. */}
+      <Button onPress={handleClick} title={'Scroll to top'} />
     </SafeAreaView>
   );
 };
