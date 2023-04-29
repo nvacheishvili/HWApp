f8c7762eb995d4d79acc8305f55902a776a93c01
Nata Vacheishvili <natashkela@gmail.com>
2023-04-29 19:14:52 -0400

    component lifecycle methods explained

--- App.js
+++ App.js
@@ -13,6 +13,56 @@ class App extends Component {
     this.state = {};
   }
 
+  // Define a "componentDidMount" method that gets called after the component is mounted to the DOM
+  componentDidMount() {
+    console.log('Component is mounted');
+  }
+
+  /**
+   * Defines a "shouldComponentUpdate" method that gets called before the component is updated
+   * @param nextProps - the props that the component will receive if it updates (read only)
+   * @param nextState - the state that the component will receive if it updates (read only)
+   * @param nextContext -  the context that the component will receive if it updates (read only)
+   * @returns {boolean} -  Return true to allow the component to update, or false to prevent it from updating
+   */
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    // Log a message that indicates we're checking if the component should update
+    console.log('Checking if component should update');
+    //Allows updating the component
+    return true;
+  }
+
+  /**
+   * Define a "getSnapshotBeforeUpdate" method that gets called right before the component is updated
+   * @param prevProps - the props that the component had before the update (read only)
+   * @param prevState - the state that the component had before the update (read only)
+   * @returns {null}
+   */
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    // Log a message that indicates we're getting a snapshot of the component before it updates
+    console.log('Getting snapshot before component updates');
+    return null;
+  }
+
+  /***
+   *  Define a "componentDidUpdate" method that gets called after the component is updated
+   * @param prevProps  - the props that the component had before the update (read only)
+   * @param prevState  - the state that the component had before the update (read only)
+   * @param snapshot  - the value returned by getSnapshotBeforeUpdate
+   */
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    // Log a message that indicates the component has updated
+    console.log('Component has updated');
+  }
+
+  /**
+   * Define a "componentWillUnmount" method that gets called right before the component is unmounted from the DOM
+   */
+  componentWillUnmount() {
+    // Log a message that indicates the component will unmount
+    console.log('Component will unmount');
+  }
+
   // Define a "render" method that returns a JSX element
   render() {
     return (
