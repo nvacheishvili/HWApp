f77cb5692773c4a975cc6117740c782973d19859
Nata Vacheishvili <natashkela@gmail.com>
2023-04-30 18:07:46 -0400

    scrollview done

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, ScrollView, Image} from 'react-native';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>Hello World!</Text>
+      <ScrollView
+        // Set the onScroll function to log a message when scrolling occurs
+        onScroll={() => console.log('We are now scrolling')}
+        // Set the contentContainerStyle to have a red background color and a fixed height of 600
+        contentContainerStyle={{backgroundColor: 'red', height: 600}}
+        // Set showsVerticalScrollIndicator to false to hide the vertical scroll indicator
+        showsVerticalScrollIndicator={false}
+        // Set horizontal to true to allow horizontal scrolling
+        horizontal={true}
+        // Set showsHorizontalScrollIndicator to false to hide the horizontal scroll indicator
+        showsHorizontalScrollIndicator={false}>
+        <Image
+          // Set the source of the image to a local image
+          source={require('./assets/images/cake.png')}
+          // Set the height and width of the image to be 500 each
+          style={{height: 500, width: 500}}
+        />
+        <Image
+          // Set the source of the image to a local image
+          source={require('./assets/images/cake.png')}
+          // Set the height and width of the image to be 500 each
+          style={{height: 500, width: 500}}
+        />
+        <Image
+          // Set the source of the image to a local image
+          source={require('./assets/images/cake.png')}
+          // Set the height and width of the image to be 500 each
+          style={{height: 500, width: 500}}
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 };
